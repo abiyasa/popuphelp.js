6ddf85fb176917e00719aa6fcf5e79b7fb6d877d
abiyasa <ilham.suhardi@gmail.com>
2012-07-02 15:49:28 +0200

    add popuphelp files and add content to index.html

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
-console.log('This would be the main JS file.');
+function showPopupHelp(targetUrl, popupWidth, popupHeight) {
+    if (!popupWidth) {
+        popupWidth = 968;
+    }
+    if (!popupHeight) {
+        popupHeight = 600;
+    }
+
+    var arguments = 'width=' + popupWidth + ',height=' + popupHeight;
+    var popupName = 'popuphelp';
+    var previewWindow = window.open(targetUrl, popupName, arguments);
+    if (previewWindow) {
+        previewWindow.focus();
+    }
+}
